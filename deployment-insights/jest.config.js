8d8238eead4b56a9f332067bca03441f64319017
Kavya <mgkavya.31@gmail.com>
2026-05-23 11:52:24 +0200

    Polish developer workflow and Docker configuration

--- deployment-insights/jest.config.js
+++ deployment-insights/jest.config.js
@@ -1,11 +1,5 @@
-import { createDefaultPreset } from "ts-jest";
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    ...tsJestTransformCfg,
-  },
+  preset: "ts-jest",
+  testEnvironment: "node"
 };
